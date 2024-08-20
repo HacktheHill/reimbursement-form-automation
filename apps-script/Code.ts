5e63b97ba68518de3d9af1d1c22732ae4a293968
Daniel Thorp <arcanistzed@gmail.com>
2024-08-20 19:15:03 +0200

    feat: improved logging
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- apps-script/Code.ts
+++ apps-script/Code.ts
@@ -3,6 +3,7 @@ declare const OAuth2: any;
 const BASE_URL = "https://quickbooks.api.intuit.com/v3/company/";
 const API_SCOPE = "com.intuit.quickbooks.accounting";
 
+// Define necessary types for responses from the QuickBooks API
 type VendorQueryResponse = {
 	QueryResponse: {
 		startPosition: number;
@@ -195,237 +196,294 @@ function onFormSubmit(e: GoogleAppsScript.Events.FormsOnFormSubmit) {
 		const bill = createBill(responseData);
 		sendDiscordWebhook(prepareWebhookPayload(responseData, bill.Bill.Id));
 	} catch (err) {
+		logError(err as Error, `Error in onFormSubmit`, { responseData });
 		// Send a Discord webhook with the error message
 		sendDiscordWebhook({
 			content: `Error: ${err instanceof Error ? err.message : err}`,
 		});
-		throw err;
 	}
 }
 
 //#region Helpers
 
 function getVendor(responseData: ResponseData) {
-	// Query the QuickBooks API to find the vendor
-	const vendorQuery = `SELECT * FROM Vendor WHERE DisplayName = '${responseData["Full Name"]}'`;
-	const VendorQueryResponse = fetchJSON(
-		`${BASE_URL}${getEnv(
-			"QUICKBOOKS_COMPANY_ID",
-		)}/query?query=${encodeURIComponent(vendorQuery)}`,
-		{
-			method: "get",
-		},
-	) as VendorQueryResponse;
-	const vendors = VendorQueryResponse.QueryResponse.Vendor ?? [];
+	try {
+		// Query the QuickBooks API to find the vendor
+		const vendorQuery = `SELECT * FROM Vendor WHERE DisplayName = '${responseData["Full Name"]}'`;
+		const VendorQueryResponse = fetchJSON(
+			`${BASE_URL}${getEnv(
+				"QUICKBOOKS_COMPANY_ID",
+			)}/query?query=${encodeURIComponent(vendorQuery)}`,
+			{ method: "get" },
+		) as VendorQueryResponse;
 
-	// Create the vendor if it doesn't exist
-	if (vendors.length === 0) {
-		vendors[0] = fetchJSON(
-			`${BASE_URL}${getEnv("QUICKBOOKS_COMPANY_ID")}/vendor`,
+		const vendors = VendorQueryResponse.QueryResponse.Vendor ?? [];
+
+		// Create the vendor if it doesn't exist
+		if (vendors.length === 0) {
+			const newVendor = fetchJSON(
+				`${BASE_URL}${getEnv("QUICKBOOKS_COMPANY_ID")}/vendor`,
+				{
+					method: "post",
+					contentType: "application/json",
+					payload: JSON.stringify({
+						PrimaryEmailAddr: {
+							Address: responseData["Email Address"],
+						},
+						DisplayName: responseData["Full Name"],
+					}),
+				},
+			);
+			Logger.log(`Vendor created: ${responseData["Full Name"]}`);
+			return newVendor;
+		} else if (vendors.length > 1) {
+			throw new Error(
+				`Multiple vendors found for name: ${responseData["Full Name"]}`,
+			);
+		} else {
+			Logger.log(`Vendor found: ${responseData["Full Name"]}`);
+			return vendors[0];
+		}
+	} catch (err) {
+		logError(err as Error, "Error fetching/creating vendor", {
+			responseData,
+		});
+		throw err;
+	}
+}
+
+function getAccount(responseData: ResponseData) {
+	try {
+		// Query the QuickBooks API to get the account
+		const accountQuery = `SELECT * FROM Account WHERE AccountType = 'Expense'`;
+		const accountQueryResponse = fetchJSON(
+			`${BASE_URL}${getEnv(
+				"QUICKBOOKS_COMPANY_ID",
+			)}/query?query=${encodeURIComponent(accountQuery)}`,
+			{ method: "get" },
+		) as AccountQueryResponse;
+
+		// Filter the accounts by account number
+		const accounts = accountQueryResponse.QueryResponse.Account.filter(
+			account => account.AcctNum === responseData["Account Number"],
+		);
+
+		// Throw an error if no account is found or multiple accounts are found
+		if (accounts.length === 0) {
+			throw new Error(
+				`Account not found: ${responseData["Account Number"]}`,
+			);
+		} else if (accounts.length > 1) {
+			throw new Error(
+				`Multiple accounts found: ${responseData["Account Number"]}`,
+			);
+		}
+
+		Logger.log(`Account found: ${responseData["Account Number"]}`);
+		return accounts[0];
+	} catch (err) {
+		logError(err as Error, "Error fetching account", { responseData });
+		throw err;
+	}
+}
+
+function getClass(responseData: ResponseData) {
+	try {
+		// Query the QuickBooks API to get the class
+		const classQuery = `SELECT * FROM Class WHERE Name = '${responseData["Class"]}'`;
+		const ClassQueryResponse = fetchJSON(
+			`${BASE_URL}${getEnv(
+				"QUICKBOOKS_COMPANY_ID",
+			)}/query?query=${encodeURIComponent(classQuery)}`,
+			{ method: "get" },
+		) as ClassQueryResponse;
+
+		const classes = ClassQueryResponse.QueryResponse.Class;
+
+		// Throw an error if no class is found or multiple classes are found
+		if (classes.length === 0) {
+			throw new Error(`Class not found: ${responseData["Class"]}`);
+		} else if (classes.length > 1) {
+			throw new Error(`Multiple classes found: ${responseData["Class"]}`);
+		}
+
+		Logger.log(`Class found: ${responseData["Class"]}`);
+		return classes[0];
+	} catch (err) {
+		logError(err as Error, "Error fetching class", { responseData });
+		throw err;
+	}
+}
+
+function getNextBillNumber() {
+	try {
+		// Query the QuickBooks API to get the latest bill number
+		const billQuery = `SELECT * FROM Bill ORDER BY DocNumber DESC`;
+		const billQueryResponse = fetchJSON(
+			`${BASE_URL}${getEnv(
+				"QUICKBOOKS_COMPANY_ID",
+			)}/query?query=${encodeURIComponent(billQuery)}`,
+			{ method: "get" },
+		) as BillQueryResponse;
+
+		const bills = billQueryResponse.QueryResponse.Bill ?? [];
+
+		// Get the latest correctly formatted bill number
+		const latestBillNumber = bills.find(bill =>
+			/^\d{6}$/.test(bill.DocNumber),
+		)?.DocNumber;
+
+		// Throw an error if no bill is found
+		if (!latestBillNumber) {
+			throw new Error(
+				"No bill found from which to get the next bill number",
+			);
+		}
+
+		// Increment the bill number
+		const nextBillNumber = (parseInt(latestBillNumber) + 1)
+			.toString()
+			.padStart(6, "0");
+		return nextBillNumber;
+	} catch (err) {
+		logError(err as Error, "Error getting next bill number");
+		throw err;
+	}
+}
+
+function uploadReceipt(fileId: string, billId: string) {
+	try {
+		// Retrieve the file from Google Drive
+		const file = DriveApp.getFileById(fileId);
+		const fileBlob = file.getBlob();
+
+		// Metadata for the receipt attachment
+		const metadata = {
+			AttachableRef: [
+				{
+					EntityRef: {
+						type: "Bill",
+						value: billId,
+					},
+				},
+			],
+			FileName: file.getName(),
+			ContentType: fileBlob.getContentType(),
+		};
+
+		// Prepare the payload as multipart/form-data
+		const boundary = "-------314159265358979323846";
+		const endBoundary = `\r\n--${boundary}--\r\n`;
+
+		const payload = `--${boundary}\r\nContent-Disposition: form-data; name="file_metadata_01"; filename="metadata.json"\r\nContent-Type: application/json; charset=UTF-8\r\n\r\n${JSON.stringify(
+			metadata,
+		)}\r\n--${boundary}\r\nContent-Disposition: form-data; name="file_content_01"; filename="${file.getName()}"\r\nContent-Type: ${fileBlob.getContentType()}\r\n\r\n`;
+
+		const postData = Utilities.newBlob(payload).getBytes();
+		const fileData = fileBlob.getBytes();
+		const endData = Utilities.newBlob(endBoundary).getBytes();
+
+		// Combine payload and file data
+		const fullPayload = Utilities.newBlob(
+			postData.concat(fileData).concat(endData),
+		).getBytes();
+
+		// Upload the file to QuickBooks
+		fetchJSON(`${BASE_URL}${getEnv("QUICKBOOKS_COMPANY_ID")}/upload`, {
+			method: "post",
+			contentType: `multipart/form-data; boundary=${boundary}`,
+			payload: fullPayload,
+		});
+
+		Logger.log(`Receipt uploaded: ${file.getName()}`);
+	} catch (err) {
+		logError(err as Error, "Error uploading receipt", { fileId, billId });
+		throw err;
+	}
+}
+
+function createBill(responseData: ResponseData) {
+	try {
+		const vendorRef = getVendor(responseData);
+		if (!vendorRef?.Id) {
+			throw new Error(
+				`Vendor reference is missing or invalid: ${JSON.stringify(
+					vendorRef,
+				)}`,
+			);
+		}
+
+		const accountRef = getAccount(responseData);
+		if (!accountRef?.Id) {
+			throw new Error(
+				`Account reference is missing or invalid: ${JSON.stringify(
+					accountRef,
+				)}`,
+			);
+		}
+
+		const classRef = getClass(responseData);
+		if (!classRef?.Id) {
+			throw new Error(
+				`Class reference is missing or invalid: ${JSON.stringify(
+					classRef,
+				)}`,
+			);
+		}
+
+		const billNumber = getNextBillNumber();
+
+		const bill = fetchJSON(
+			`${BASE_URL}${getEnv("QUICKBOOKS_COMPANY_ID")}/bill`,
 			{
 				method: "post",
 				contentType: "application/json",
 				payload: JSON.stringify({
-					PrimaryEmailAddr: {
-						Address: responseData["Email Address"],
+					VendorRef: {
+						value: vendorRef.Id,
 					},
-					DisplayName: responseData["Full Name"],
+					TxnDate: responseData["Transaction Date"],
+					Line: [
+						{
+							DetailType: "AccountBasedExpenseLineDetail",
+							Amount: responseData["Amount"],
+							AccountBasedExpenseLineDetail: {
+								AccountRef: {
+									value: accountRef.Id,
+								},
+								ClassRef: {
+									value: classRef.Id,
+								},
+							},
+							Description: responseData["Description"],
+						},
+					],
+					CurrencyRef: {
+						value: "CAD",
+					},
+					DocNumber: billNumber,
 				}),
 			},
 		);
-		Logger.log(`Vendor created: ${responseData["Full Name"]}`);
-	} else if (vendors.length > 1) {
-		throw new Error(`Multiple vendors found: ${responseData["Full Name"]}`);
+
+		if (!bill || !bill.Bill) {
+			throw new Error(
+				`Bill creation failed: ${JSON.stringify(bill, null, 2)}`,
+			);
+		} else {
+			Logger.log(
+				`Bill created successfully: ${billNumber} - ${responseData["Full Name"]} - ${responseData["Amount"]} - ${responseData["Description"]}`,
+			);
+		}
+
+		// Upload the receipt
+		uploadReceipt(responseData["Receipt"], bill.Bill.Id);
+
+		return bill;
+	} catch (err) {
+		logError(err as Error, "Error creating bill", { responseData });
+		throw err;
 	}
-
-	// Log the vendor
-	const vendorRef = vendors[0];
-	Logger.log(`Vendor: ${responseData["Full Name"]}`);
-
-	return vendorRef;
-}
-
-function getAccount(responseData: ResponseData) {
-	// Query the QuickBooks API to get the account
-	const accountQuery = `SELECT * FROM Account WHERE AccountType = 'Expense'`;
-	const accountQueryResponse = fetchJSON(
-		`${BASE_URL}${getEnv(
-			"QUICKBOOKS_COMPANY_ID",
-		)}/query?query=${encodeURIComponent(accountQuery)}`,
-		{
-			method: "get",
-		},
-	) as AccountQueryResponse;
-
-	// Filter the accounts by account number
-	const accounts = accountQueryResponse.QueryResponse.Account.filter(
-		account => account.AcctNum === responseData["Account Number"],
-	);
-
-	// Throw an error if no account is found or multiple accounts are found
-	if (accounts.length === 0) {
-		throw new Error(`Account not found: ${responseData["Account Number"]}`);
-	} else if (accounts.length > 1) {
-		throw new Error(
-			`Multiple accounts found: ${responseData["Account Number"]}`,
-		);
-	}
-
-	// Log the account
-	const accountRef = accounts[0];
-	Logger.log(`Account: ${responseData["Account Number"]}`);
-
-	return accountRef;
-}
-
-function getClass(responseData: ResponseData) {
-	// Query the QuickBooks API to get the class
-	const classQuery = `SELECT * FROM Class WHERE Name = '${responseData["Class"]}'`;
-	const ClassQueryResponse = fetchJSON(
-		`${BASE_URL}${getEnv(
-			"QUICKBOOKS_COMPANY_ID",
-		)}/query?query=${encodeURIComponent(classQuery)}`,
-		{
-			method: "get",
-		},
-	) as ClassQueryResponse;
-	const classes = ClassQueryResponse.QueryResponse.Class;
-
-	// Throw an error if no class is found or multiple classes are found
-	if (classes.length === 0) {
-		throw new Error(`Class not found: ${responseData["Class"]}`);
-	} else if (classes.length > 1) {
-		throw new Error(`Multiple classes found: ${responseData["Class"]}`);
-	}
-
-	// Log the class
-	const classRef = classes[0];
-	Logger.log(`Class: ${responseData["Class"]}`);
-
-	return classRef;
-}
-
-function getNextBillNumber() {
-	const billQuery = `SELECT * FROM Bill ORDER BY DocNumber DESC`;
-	const billQueryResponse = fetchJSON(
-		`${BASE_URL}${getEnv(
-			"QUICKBOOKS_COMPANY_ID",
-		)}/query?query=${encodeURIComponent(billQuery)}`,
-		{
-			method: "get",
-		},
-	) as BillQueryResponse;
-	const bills = billQueryResponse.QueryResponse.Bill ?? [];
-
-	// Get the latest correctly formatted bill number
-	const latestBillNumber = bills.find(bill => {
-		return /^\d{6}$/.test(bill.DocNumber);
-	})?.DocNumber;
-
-	// Throw an error if no bill is found
-	if (!latestBillNumber) {
-		throw new Error("No bill found from which to get the next bill number");
-	}
-
-	// Increment the bill number
-	const nextBillNumber = (parseInt(latestBillNumber) + 1)
-		.toString()
-		.padStart(6, "0");
-
-	return nextBillNumber;
-}
-
-function uploadReceipt(fileId: string, billId: string) {
-	// Retrieve the file from Google Drive
-	const file = DriveApp.getFileById(fileId);
-	const fileBlob = file.getBlob();
-
-	const metadata = {
-		AttachableRef: [
-			{
-				EntityRef: {
-					type: "Bill",
-					value: billId,
-				},
-			},
-		],
-		FileName: file.getName(),
-		ContentType: fileBlob.getContentType(),
-	};
-
-	// Prepare the payload as multipart/form-data
-	const boundary = "-------314159265358979323846";
-	const endBoundary = `\r\n--${boundary}--\r\n`;
-
-	const payload = `--${boundary}\r\nContent-Disposition: form-data; name="file_metadata_01"; filename="metadata.json"\r\nContent-Type: application/json; charset=UTF-8\r\n\r\n${JSON.stringify(
-		metadata,
-	)}\r\n--${boundary}\r\nContent-Disposition: form-data; name="file_content_01"; filename="${file.getName()}"\r\nContent-Type: ${fileBlob.getContentType()}\r\n\r\n`;
-
-	const postData = Utilities.newBlob(payload).getBytes();
-	const fileData = fileBlob.getBytes();
-	const endData = Utilities.newBlob(endBoundary).getBytes();
-
-	const fullPayload = Utilities.newBlob(
-		postData.concat(fileData).concat(endData),
-	).getBytes();
-
-	// Upload the file to QuickBooks
-	fetchJSON(`${BASE_URL}${getEnv("QUICKBOOKS_COMPANY_ID")}/upload`, {
-		method: "post",
-		contentType: `multipart/form-data; boundary=${boundary}`,
-		payload: fullPayload,
-	});
-
-	Logger.log(`Receipt uploaded: ${file.getName()}`);
-}
-
-function createBill(responseData: ResponseData) {
-	const vendorRef = getVendor(responseData);
-	const accountRef = getAccount(responseData);
-	const classRef = getClass(responseData);
-	const billNumber = getNextBillNumber();
-
-	const bill = fetchJSON(
-		`${BASE_URL}${getEnv("QUICKBOOKS_COMPANY_ID")}/bill`,
-		{
-			method: "post",
-			contentType: "application/json",
-			payload: JSON.stringify({
-				VendorRef: {
-					value: vendorRef.Id,
-				},
-				TxnDate: responseData["Transaction Date"],
-				Line: [
-					{
-						DetailType: "AccountBasedExpenseLineDetail",
-						Amount: responseData["Amount"],
-						AccountBasedExpenseLineDetail: {
-							AccountRef: {
-								value: accountRef.Id,
-							},
-							ClassRef: {
-								value: classRef.Id,
-							},
-						},
-						Description: responseData["Description"],
-					},
-				],
-				CurrencyRef: {
-					value: "CAD",
-				},
-				DocNumber: billNumber,
-			}),
-		},
-	);
-
-	Logger.log(
-		`Bill created: ${billNumber} - ${responseData["Full Name"]} - ${responseData["Amount"]} - ${responseData["Description"]}`,
-	);
-
-	// Upload the receipt
-	uploadReceipt(responseData["Receipt"], bill.Bill.Id);
-
-	return bill;
 }
 
 function prepareWebhookPayload(responseData: ResponseData, billId: string) {
@@ -607,7 +665,10 @@ function fetchJSON(
 	try {
 		return JSON.parse(response.getContentText());
 	} catch (err) {
-		return response.getContentText();
+		Logger.log(
+			`Error parsing response from URL: ${url}, Response: ${response.getContentText()}`,
+		);
+		throw err;
 	}
 }
 
@@ -633,6 +694,15 @@ function isValidResponseData(responseData: any): responseData is ResponseData {
 	}
 
 	return true;
+}
+
+function logError(error: Error, contextMessage: string, data?: any) {
+	Logger.log(
+		`ERROR: ${contextMessage}\nMessage: ${error.message}\nStack: ${error.stack}`,
+	);
+	if (data) {
+		Logger.log(`Additional data: ${JSON.stringify(data, null, 2)}`);
+	}
 }
 
 //#endregion Utilities
